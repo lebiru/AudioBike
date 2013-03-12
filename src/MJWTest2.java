b75375a36101ba51e93e55bc4a63fe877c6c480b
biru <babdulnabi.student@manhattan.edu>
2013-03-12 16:28:33 -0400

    Demo Level complete, working on Bike

--- src/MJWTest2.java
+++ src/MJWTest2.java
@@ -1,9 +1,11 @@
+import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.collision.shapes.PolygonShape;
 
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.testbed.framework.TestbedTest;
 
 public class MJWTest2 extends TestbedTest {
@@ -16,39 +18,96 @@ public class MJWTest2 extends TestbedTest {
 		getWorld().setGravity(new Vec2(0.0f, -10.0f));
 		boolean doSleep = true;
 		getWorld().setAllowSleep(doSleep);
-		
-		BodyDef groundBodyDef = new BodyDef(); // body definition
-		groundBodyDef.position.set(0.0f, -10.0f); // set bodydef position
-		Body groundBody = getWorld().createBody(groundBodyDef); // create body based on definition
-		PolygonShape groundBox = new PolygonShape(); // make a shape representing ground
-		groundBox.setAsBox(50.0f, 10.0f); // shape is a rect: 100 wide, 20 high
-		groundBody.createFixture(groundBox, 0.0f); // bind shape to ground body
-		
-		BodyDef leftWall = new BodyDef();
-		leftWall.position.set(-20.0f, 0.0f);
-		Body leftWallBody = getWorld().createBody(leftWall);
-		PolygonShape leftWallBox = new PolygonShape();
-		leftWallBox.setAsBox(5.0f, 100.0f);
-		leftWallBody.createFixture(leftWallBox, 0.0f);
 
-		
-		
+		makeVehicle();
+		makeLevel();
 
-		for (int i = 0; i < 30; i++) {
-			PolygonShape polygonShape = new PolygonShape();
-			polygonShape.setAsBox(1, 1);
+	}
 
-			BodyDef bodyDef = new BodyDef();
-			bodyDef.type = BodyType.DYNAMIC;
-			bodyDef.position.set(5 * i, 0);
-			bodyDef.angle = (float) (Math.PI / 4 * i);
-			bodyDef.allowSleep = false;
-			Body body = getWorld().createBody(bodyDef);
-			body.createFixture(polygonShape, 5.0f);
+	private void makeVehicle() 
+	{
+			
 
-			body.applyForce(new Vec2(-10000 * (i - 1), 0), new Vec2());
+	}
+
+	private void makeLevel() {
+
+		BodyDef b = new BodyDef();
+		b.position.set(0.0f, 0.5f);
+
+		PolygonShape bbox = new PolygonShape();
+		bbox.setAsBox(50.0f, 0.5f);
+
+
+
+		Body body = getWorld().createBody(b);
+		FixtureDef fixtureDef = new FixtureDef();
+
+		bbox.setAsBox(1.0f, 2.0f, new Vec2(-50.0f, 0.5f), 0.0f);
+		fixtureDef.shape = bbox;
+		body.createFixture(fixtureDef);
+
+		bbox.setAsBox(1.0f, 2.0f, new Vec2(50.0f, 0.5f), 0.0f);
+		fixtureDef.shape = bbox;
+		body.createFixture(fixtureDef);
+
+		bbox.setAsBox(50.0f, 1.0f, new Vec2(0.0f, 0.5f), 0.0f);
+		fixtureDef.shape = bbox;
+		body.createFixture(fixtureDef);
+
+		bbox.setAsBox(3.0f, 0.5f, new Vec2(5.0f, 1.5f), (float)Math.PI/4);
+		fixtureDef.shape = bbox;
+		body.createFixture(fixtureDef);
+
+		bbox.setAsBox(3.0f, 0.5f, new Vec2(3.5f, 1.0f), (float)Math.PI/8);
+		fixtureDef.shape = bbox;
+		body.createFixture(fixtureDef);
+
+		bbox.setAsBox(3.0f, 0.5f, new Vec2(9.0f, 1.5f), (float)-Math.PI/4);
+		fixtureDef.shape = bbox;
+		body.createFixture(fixtureDef);
+
+		bbox.setAsBox(3.0f, 0.5f, new Vec2(10.5f, 1.0f), (float)-Math.PI/8);
+		fixtureDef.shape = bbox;
+		body.createFixture(fixtureDef);
+
+		makeParticles();
+
+
+		body.resetMassData();
+
+	}
+
+	private void makeParticles() 
+	{
+
+
+		for (int i = 0; i < 30; i++) 
+		{
+
+			PolygonShape dynamicBox = new PolygonShape();
+			dynamicBox.setAsBox(0.05f, 0.05f);
+
+			FixtureDef fd = new FixtureDef();
+			fd.density = 0.01f;
+			fd.friction = 0.1f;
+			fd.restitution = 0.5f;
+			dynamicBox.m_radius = (float) ((float) Math.random()/20+0.02);
+			fd.shape = dynamicBox;
+
+			BodyDef bd = new BodyDef();
+			bd.type = BodyType.DYNAMIC;
+			bd.position.set((float)Math.random()*35+15, 3.0f);
+			bd.allowSleep = true;
+			bd.linearDamping = 0.1f;
+			bd.angularDamping = 0.1f;
+
+
+			Body body = getWorld().createBody(bd);
+			body.createFixture(fd);
+			body.resetMassData();
 		}
-		
+
 	}
 
 	@Override
